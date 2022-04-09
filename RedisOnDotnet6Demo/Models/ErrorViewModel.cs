daf758a01258eaccda243ca104eaf4bc1a45dc1a
Nishan Wickramarathna <nishanw@99x.io>
2022-04-10 04:03:14 +0530

    first commit

---
+++ RedisOnDotnet6Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RedisOnDotnet6Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
