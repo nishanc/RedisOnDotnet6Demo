a874f5dec5188f49bdada598ca89b1f72b62a6bc
Nishan Wickramarathna <nishanw@99x.io>
2022-04-16 00:59:25 +0530

    cache helpers

---
+++ RedisOnDotnet6Demo/Helpers/CacheHelper.cs
@@ -0,0 +1,36 @@
+using System.Text.Json;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace RedisOnDotnet6Demo.Helpers
+{
+    public static class CacheHelper
+    {
+        public static async Task SetRecordAsync<T>(this IDistributedCache cache,
+                                                   string recordId,
+                                                   T data,
+                                                   TimeSpan? absoluteExpireTime = null,
+                                                   TimeSpan? slidingExpireTime = null)
+        {
+            var options = new DistributedCacheEntryOptions();
+
+            options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60);
+            options.SlidingExpiration = slidingExpireTime;
+
+            var jsonData = JsonSerializer.Serialize(data);
+            await cache.SetStringAsync(recordId, jsonData, options);
+        }
+
+        public static async Task<T?> GetRecordAsync<T>(this IDistributedCache cache,
+                                                       string recordId)
+        {
+            var jsonData = await cache.GetStringAsync(recordId);
+
+            if (jsonData is null)
+            {
+                return default(T);
+            }
+
+            return JsonSerializer.Deserialize<T>(jsonData);
+        }
+    }
+}
